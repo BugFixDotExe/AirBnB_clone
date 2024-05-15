f1db9045f7405b515438ae06b0c72c81be381c09
Eunice George <georgeunice408@gmail.com>
2024-05-15 00:39:01 -0700

    task 0

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,32 @@
 import uuid
-import datetime
-import time
+from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
         else:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 elif key == "created_at":
-                    #setattr(self, "created_at", "asdsadas")
-                    self.created_at = time.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, "created_at", datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f"))
                 elif key == "updated_at":
-                    self.updated_at = time.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, "updated_at", datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f"))
                 elif key == "id":
-                    self.id = value
+                    setattr(self, "id", value)
                 elif key == "name":
-                    self.name = value
+                    setattr(self, "name", value)
                 elif key == "my_number":
-                    self.my_number = value
+                    setattr(self, "my_number", value)
 
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all
