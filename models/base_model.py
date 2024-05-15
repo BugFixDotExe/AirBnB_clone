6946c4b800b46b053d25fbb07ec8ec9610f75f7e
Eunice George <georgeunice408@gmail.com>
2024-05-15 08:22:05 -0700

    task

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,18 @@ from engine.file_storage import FileStorage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Attributes:
+            id (str): Unique ID for the instance.
+            created_at (datetime): Date and time of creation.
+            updated_at (datetime): Date and time of last update.
+        """
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -26,21 +38,31 @@ class BaseModel:
                     setattr(self, "my_number", value)
 
     def save(self):
-        """updates the public instance attribute
-        updated_at with the current datetime"""
+        """
+        Updates the public instance attribute 'updated_at' with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all
-        keys/values of __dict__ of the instance"""
+        """
+        Returns a dictionary containing all keys/values of '__dict__' of the instance.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
         self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         setattr(self, "created_at", self.created_at)
         setattr(self, "updated_at", self.created_at)
         self.__dict__["__class__"] = self.__class__.__name__
-        # setattr(self, str(self.__str__), self.__class__.__name__)
         return self.__dict__
 
     def __str__(self):
+        """
+        Returns a string representation of the instance.
+
+        Returns:
+            str: String representation of the instance.
+        """
         return "[{:s}] ({:s} {})".format(self.__class__.__name__,
                                          self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,45 @@
 import os
 
 class FileStorage:
+    """Class for storing and managing objects in a JSON file.
+
+    Attributes:
+        __file_path (str): The path to the JSON file where objects are serialized.
+        __objects (dict): A dictionary to store serialized objects.
+
+    Methods:
+        all(self): Return a dictionary of all stored objects.
+        new(self, obj): Add a new object to the storage dictionary.
+        save(self): Save the serialized objects to the JSON file.
+        reload(self): Load serialized objects from the JSON file.
+    """
+
     __file_path = None
     __objects = dict()
 
     def all(self):
+        """Return a dictionary of all objects currently stored."""
+
         print("Well this is rather fitting.")
     def new(self, obj):
+        """Add a new object to the storage dictionary.
+
+        Args:
+            obj: The object to be added to the storage dictionary.
+        """
         pass
+
     def save(self):
+        """
+        save(self): Save the serialized objects to the JSON file.
+        """        
         with open(self.__file_path, mode="w", encoding="UTF-8") as file_s:
             json.dump(self.__objects, file_s)
+
     def reload(self):
+        """
+        reload(self): Load serialized objects from the JSON file.
+        """
         if os.path.exist(self.__file_path):
             with open(self.__file_path, mode="r", encoding="UTF-8") as file_s:
                 self.__objects = dict(json.load(file_s)).copy()
